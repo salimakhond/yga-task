fe7104f577104f1a64655e8bd6f0247c966b4513
main <salimakhond@gmai.com>
2023-08-28 09:55:45 +0600

    complete YGA task project

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
+// menuToggle
 const menuToggle = document.getElementById("menuToggle");
 const navList = document.getElementById("navList");
 
 menuToggle.addEventListener("click", () => {
   navList.classList.toggle("menu-open");
 });
-
-
-
-
