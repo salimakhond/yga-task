b9abe4164e56ee3c126ab987f17368b286de454d
main <salimakhond@gmai.com>
2023-08-28 08:51:24 +0600

    done

---
+++ js/active.js
@@ -0,0 +1,23 @@
+/*
+Template Name: Massive
+Author: GrayGrids
+*/
+
+// (function () {
+
+// 	/*=======================
+// 		03.  Home Slider JS
+// 		=========================*/
+// 	$('.slider-active').owlCarousel({
+// 		items: 1,
+// 		autoplay: false,
+// 		autoplayTimeout: 4500,
+// 		smartSpeed: 500,
+// 		autoplayHoverPause: false,
+// 		loop: true,
+// 		nav: true,
+// 		dots: false,
+// 		navText: ['<i class="icofont-simple-left"></i>', '<i class="icofont-simple-right"></i>'],
+// 	});
+
+// })();
---
+++ js/script.js
@@ -0,0 +1,10 @@
+const menuToggle = document.getElementById("menuToggle");
+const navList = document.getElementById("navList");
+
+menuToggle.addEventListener("click", () => {
+  navList.classList.toggle("menu-open");
+});
+
+
+
+
